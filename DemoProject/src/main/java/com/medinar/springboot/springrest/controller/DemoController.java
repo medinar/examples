0c806107a80305888960907d2c70ce6ac255eea8
medinar <rommel.d.medina@gmail.com>
2018-01-31 23:15:40 +0800

    Test Commit
    Initial commit for demo project.

---
+++ DemoProject/src/main/java/com/medinar/springboot/springrest/controller/DemoController.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.medinar.springboot.springrest.controller;
+
+        
+/**
+ *
+ * @author corazonmedina
+ */
+public class DemoController {
+    
+}
